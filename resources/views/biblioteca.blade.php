094f70f7d352bd267b9aa5920bf1186736845eb2
Antonio Cabrera <antoniojesuscabrera6@gmail.com>
2023-02-19 18:08:36 +0100

    Biblioteca

---
+++ resources/views/biblioteca.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Biblioteca')
+
+@section('content')
+    <h1>Bienvenido a la biblioteca</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BibliotecaController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', LoginController::class);
+
+Route::get('/home', HomeController::class);
+
+Route::get('/biblioteca', BibliotecaController::class);
