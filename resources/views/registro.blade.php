dfccd86d00366f8e2a4d1bd884a64b926417712a
AntonioJCG <antoniojesuscabrera6@gmail.com>
2023-02-21 11:37:37 +0100

    Css

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>Registro</title>
     <link rel="stylesheet" href="css/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
@@ -17,7 +17,11 @@
             <div class="imgLogo mb-3">
                 <img src="../public/imgs/logo.png" alt="">
             </div>
-            <form action="" method="get">
+            <form>
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email</label>
+                  <input type="email" class="form-control" id="email">
+                </div>
                 <div class="mb-3">
                     <label for="usuario" class="form-label">Usuario</label>
                     <input type="text" class="form-control" id="usuario">
@@ -26,8 +30,8 @@
                   <label for="password" class="form-label">Contraseña</label>
                   <input type="password" class="form-control" id="password">
                 </div>
-                <button type="submit" class="btn btn-primary">Iniciar sesión</button>
-                <a href="registro"><button type="text" class="btn btn-success">Registrarse</button></a>
+                <button type="submit" class="btn btn-primary">Registrarse</button>
+                <a href="login"><button type="text" class="btn btn-success">Volver</button>
             </form>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegistroController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BibliotecaController;
 
@@ -17,6 +18,8 @@ use App\Http\Controllers\BibliotecaController;
 */
 
 Route::get('/', LoginController::class);
+
+Route::get('/registro', RegistroController::class);
 
 Route::get('/home', HomeController::class);
 
