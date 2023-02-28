ec01f30c7f3191e3845a16cc69fa837acda362cd
AntonioJCG <antoniojesuscabrera6@gmail.com>
2023-02-28 20:29:24 +0100

    CSS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,31 +9,35 @@
     </div>
     <div class="container-fluid d-flex flex-column mb-5">
         <div class="container-fluid my-2"><h1>Top de cada género</h1><hr></div>
-        <div class="container-fluid bg-success d-flex justify-content-around">
-            <div class="container-fluid bg-danger m-4">
-                Libro
+        <div class="libros container-fluid bg-success d-flex justify-content-around">
+            <div class="container-fluid d-flex flex-column bg-danger m-2 p-2">
+                <div class="container-fluid text-center">
+                    <p class="titulo-libro">De sangre y cenizas</p>
+                </div>
+                <div class="container-fluid d-flex justify-content-center">
+                    <img src="../public/imgs/sangre.jpg" alt="" class="row container-fluid col-lg-10 col-md-12">
+                </div>
             </div>
-            <div class="container-fluid bg-danger m-4">
-                Libro
+            <div class="container-fluid d-flex flex-column bg-danger m-2 p-2">
+                <div class="container-fluid text-center">
+                    <p class="titulo-libro">De sangre y cenizas</p>
+                </div>
+                <div class="container-fluid d-flex justify-content-center">
+                    <img src="../public/imgs/sangre.jpg" alt="" class="row container-fluid col-lg-10 col-md-12">
+                </div>
             </div>
-            <div class="container-fluid bg-danger m-4">
-                Libro
-            </div>
-        </div>
-        <div class="container-fluid bg-success d-flex justify-content-around mb-4">
-            <div class="container-fluid bg-danger m-4">
-                Libro
-            </div>
-            <div class="container-fluid bg-danger m-4">
-                Libro
-            </div>
-            <div class="container-fluid bg-danger m-4">
-                Libro
+            <div class="container-fluid d-flex flex-column bg-danger m-2 p-2">
+                <div class="container-fluid text-center">
+                    <p class="titulo-libro">De sangre y cenizas</p>
+                </div>
+                <div class="container-fluid d-flex justify-content-center">
+                    <img src="../public/imgs/sangre.jpg" alt="" class="row container-fluid col-lg-10 col-md-12">
+                </div>
             </div>
         </div>
     </div>
     <div class="container-fluid d-flex flex-column mb-5">
-        <div class="container-fluid my-2"><h1>Últimos libros añadidos</h1><hr></div>
+        <div class="container-fluid my-2"><h1>Últimos libros publicados</h1><hr></div>
         <div class="container-fluid bg-success d-flex justify-content-around">
             <div class="container-fluid bg-danger m-4">
                 Libro
