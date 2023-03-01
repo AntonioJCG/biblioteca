4f93fbd484992e4a360605644c15220298c4cd59
AntonioJCG <antoniojesuscabrera6@gmail.com>
2023-03-01 10:18:24 +0100

    CSS

--- resources/views/biblioteca.blade.php
+++ resources/views/biblioteca.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.plantilla')
 
-@section('title', 'Biblioteca')
+@section('title', 'ILoveBooks - Biblioteca')
 
 @section('content')
-    <h1>Bienvenido a la biblioteca</h1>
+<div class="principal container bg-light rounded mx-3">
+    <div class="container-fluid text-center my-5">
+        <h1>Buscador de libros</h1>
+    </div>
+    <div class="container-fluid mb-5">
+        <form action="" method="post">
+            <div class="input-group d-flex justify-content-center">
+                <div class="form-outline">
+                  <input type="search" id="form1" class="form-control" />
+                </div>
+                <button type="submit" class="btn btn-primary">
+                  Buscar
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         <h1>¡Bienvenido/a a ILoveBooks,<br>hogar de mil historias!</h1>
     </div>
     <div class="container-fluid d-flex flex-column mb-5">
-        <div class="container-fluid my-2"><h1>Top de cada género</h1><hr></div>
+        <div class="container-fluid my-2"><h1>Libros más reservados</h1><hr></div>
         <div class="container-fluid d-flex justify-content-around">
             <div class="libro container-fluid d-flex flex-column m-2 p-2 rounded">
                 <div class="container-fluid text-center">
@@ -38,32 +38,6 @@
     </div>
     <div class="container-fluid d-flex flex-column mb-5">
         <div class="container-fluid my-2"><h1>Últimos libros publicados</h1><hr></div>
-        <div class="container-fluid d-flex justify-content-around">
-            <div class="libro container-fluid d-flex flex-column m-2 p-2 rounded">
-                <div class="container-fluid text-center">
-                    <p class="titulo-libro">De sangre y cenizas</p>
-                </div>
-                <div class="container-fluid d-flex justify-content-center">
-                    <img src="../public/imgs/sangre.jpg" alt="" class="row container-fluid col-lg-10 col-md-12">
-                </div>
-            </div>
-            <div class="libro container-fluid d-flex flex-column m-2 p-2 rounded">
-                <div class="container-fluid text-center">
-                    <p class="titulo-libro">De sangre y cenizas</p>
-                </div>
-                <div class="container-fluid d-flex justify-content-center">
-                    <img src="../public/imgs/sangre.jpg" alt="" class="row container-fluid col-lg-10 col-md-12">
-                </div>
-            </div>
-            <div class="libro container-fluid d-flex flex-column m-2 p-2 rounded">
-                <div class="container-fluid text-center">
-                    <p class="titulo-libro">De sangre y cenizas</p>
-                </div>
-                <div class="container-fluid d-flex justify-content-center">
-                    <img src="../public/imgs/sangre.jpg" alt="" class="row container-fluid col-lg-10 col-md-12">
-                </div>
-            </div>
-        </div>
         <div class="container-fluid d-flex justify-content-around">
             <div class="libro container-fluid d-flex flex-column m-2 p-2 rounded">
                 <div class="container-fluid text-center">
